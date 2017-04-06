46b8702cd5c1a17d11058f4338dacffe7b2c468a
Irene Cortinovis <irenecortinovis1@gmail.com>
2017-04-06 21:12:13 +0200

    Add transfer function code and data

---
+++ c2/src/TransfFunc.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <TH1F.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TApplication.h>
+#include <TGraphErrors.h>
+#include <TGraphPolar.h>
+#include <TGraphPolargram.h>
+
+/*
+compile:
+c++ TransfFunc.cpp `root-config --cflags --glibs` -o TransfFunc.o
+
+run:
+transfer function Va-b/Vb deltaPhi
+./TransfFunc.o 1
+
+transfer function Vb/Va deltaPhi'
+./TransfFunc.o 2
+*/
+
+
+int main(int argc, char** argv){
+
+	if(argc==1)
+	{
+		std::cout << "error: missing argument: run with argument 1 or 2" << std::endl;
+		return 1;
+	}
+
+	std::string arg1(argv[1]);
+
+	TApplication* Grafica = new TApplication("Grafica", 0, NULL);
+
+	//create vectors for tgrapherrors
+	int n=13;
+	double vba_vb[n];
+	double errv[n];
+	double deltaphi[n];
+	double errdeltaphi[n];
+
+	//fill in vectors
+	double a, b, c, d;	
+	std::ifstream inFile;
+	std::string filename = "../data/TransfFunct" + arg1 + ".txt";
+
+	inFile.open(filename.c_str(), std::ios::in);
+
+	int i = 0;
+	while(!inFile.eof())
+	{
+	    inFile >> a >> b >> c >> d;
+	    vba_vb[i] = a;
+	    errv[i] = b;
+	    deltaphi[i] = c;
+	    errdeltaphi[i] = d;
+	    i++;
+	}
+
+	inFile.close();
+
+	//plot
+	TCanvas* c1 = new TCanvas("c1","test");
+	TGraphPolar * gr = new TGraphPolar(n, deltaphi, vba_vb, errdeltaphi, errv);
+	gr->SetTitle("test");
+	gr->SetMarkerStyle(20);
+	gr->SetMarkerSize(2.);
+	gr->SetMarkerColor(4);
+	gr->SetLineColor(2);
+	gr->SetLineWidth(3);
+    gr->Draw("PEL");
+	
+
+	c1->Update();
+   	gr->GetPolargram()->SetToRadian();
+	c1->Print("R1.png", "png");
+
+
+	Grafica->Run("");
+
+	return 0;
+}
