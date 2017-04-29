c6266966e6edbf6710a22e158dff0a52345eb592
Irene Cortinovis <irenecortinovis1@gmail.com>
2017-04-29 22:29:45 +0200

    Fix units and add calculation for weighted average of tau (w/ error), C and L (w/ error)

--- c3/src/RC_impulsata.cpp
+++ c3/src/RC_impulsata.cpp
@@ -53,22 +53,22 @@ int main()
 
 	TGraphErrors * Gerr1 = new TGraphErrors("../data/RCVCcharge.txt");
 	Gerr1->SetTitle("V ai capi del condensatore durante carica");
-	Gerr1->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr1->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr1->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr2 = new TGraphErrors("../data/RCVCdischarge.txt");
 	Gerr2->SetTitle("V ai capi del condensatore durante scarica");
-	Gerr2->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr2->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr2->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr3 = new TGraphErrors("../data/RCVRcharge.txt");
 	Gerr3->SetTitle("V ai capi del resistore durante carica");
-	Gerr3->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr3->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr3->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr4 = new TGraphErrors("../data/RCVRdischarge.txt");
 	Gerr4->SetTitle("V ai capi del resistore durante scarica");
-	Gerr4->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr4->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr4->GetYaxis()->SetTitle("Tensione [V]");
 
 	
@@ -128,7 +128,37 @@ int main()
 	
 
 //--------------------------------------Esporta immagini
-	//c1->Print("sottosmorzato.eps","eps");
+	//c1->Print("C3_P2_RC_impulsata.eps","eps"
+
+
+//--------------------------------------Calcolo di tau e di C
+	//tau: mean (weighted) value and error [s]
+	double tau1 = (fit1->GetParameter(1))/1000000;
+	double wtauerr1 = 1/pow((fit1->GetParError(1))/1000000, 2);
+	double tau2 = (fit2->GetParameter(1))/1000000;
+	double wtauerr2 = 1/pow((fit2->GetParError(1))/1000000, 2);
+	double tau3 = (fit3->GetParameter(1))/1000000;
+	double wtauerr3 = 1/pow((fit3->GetParError(1))/1000000, 2);
+	double tau4 = (fit4->GetParameter(1))/1000000;
+	double wtauerr4 = 1/pow((fit4->GetParError(1))/1000000, 2);
+
+	double tau_mean = (tau1*wtauerr1+tau2*wtauerr2+tau3*wtauerr3+tau4*wtauerr4)
+						/ (wtauerr1+wtauerr2+wtauerr3+wtauerr4); 
+	double tau_err = 1/sqrt(wtauerr1+wtauerr2+wtauerr3+wtauerr4);
+
+	std::cout << "Weighted average for tau [s] = " << tau_mean <<
+				 " +/- " << tau_err << std::endl;
+
+	//resistance: value and error
+	const double resistance = 14870; //Ohm
+	double resistance_err = 1 + 14870/100*0.9; //Ohm, as in Fluke manual
+
+	//capacitance: value and error (propagation)
+	double capacitance = tau_mean/resistance;
+	double capacitance_err = sqrt(pow(tau_err/resistance,2)+
+							pow(tau_mean/(resistance*resistance)*resistance_err,2));
+
+	std::cout << "Capacitance [farad] = " << capacitance << " +/- " << capacitance_err << std::endl;
 
 
 
--- c3/src/RL_impulsata.cpp
+++ c3/src/RL_impulsata.cpp
@@ -27,22 +27,22 @@ int main()
 
 //---------------------------Definizione Fit
 
-	TF1 * fit1 = new TF1("RL VL carica", "2*[0]*exp(-x/[1])",0,700);
+	TF1 * fit1 = new TF1("RL VL -+", "2*[0]*exp(-x/[1])",0,700);
 	//[0]= V0 [1]=ta
 	fit1->SetParName(0,"V_0 [V]");
 	fit1->SetParName(1,"Tau [micro s]");
 
-	TF1 * fit2 = new TF1("RL VL scarica", "-2*[0]*exp(-x/[1])",0,700);
+	TF1 * fit2 = new TF1("RL VL +-", "-2*[0]*exp(-x/[1])",0,700);
 	//[0]= V0 [1]=tau
 	fit2->SetParName(0,"V_0 [V]");
 	fit2->SetParName(1,"Tau [micro s]");
 
-	TF1 * fit3 = new TF1("RL VL carica", "-[0]+2*[0]*(1-exp(-x/[1]))",0,700);
+	TF1 * fit3 = new TF1("RL VR -+", "[0]*(1-2*exp(-x/[1]))",0,700);
 	//[0]= V0 [1]=tau
 	fit3->SetParName(0,"V_0 [V]");
 	fit3->SetParName(1,"Tau [micro s]");
 
-	TF1 * fit4 = new TF1("RL VL scarica", "-[0]+2*[0]*exp(-x/[1])",0,700);
+	TF1 * fit4 = new TF1("RL VR +-", "-[0]*(1-2*exp(-x/[1]))",0,700);
 	//[0]= V0 [1]=tau
 	fit4->SetParName(0,"V_0 [V]");
 	fit4->SetParName(1,"Tau [micro s]");
@@ -51,23 +51,23 @@ int main()
 //-----------------Definizione grafici
 
 	TGraphErrors * Gerr1 = new TGraphErrors("../data/RLVLcharge.txt");
-	Gerr1->SetTitle("V ai capi dell'induttore durante carica");
-	Gerr1->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr1->SetTitle("V ai capi dell'induttore nel passaggio da -V0 a +V0");
+	Gerr1->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr1->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr2 = new TGraphErrors("../data/RLVLdischarge.txt");
-	Gerr2->SetTitle("V ai capi dell'induttore durante scarica");
-	Gerr2->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr2->SetTitle("V ai capi dell'induttore nel passaggio da +V0 a -V0");
+	Gerr2->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr2->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr3 = new TGraphErrors("../data/RLVRcharge.txt");
-	Gerr3->SetTitle("V ai capi del resistore durante carica");
-	Gerr3->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr3->SetTitle("V ai capi del resistore nel passaggio da -V0 a +V0");
+	Gerr3->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr3->GetYaxis()->SetTitle("Tensione [V]");
 
 	TGraphErrors * Gerr4 = new TGraphErrors("../data/RLVRdischarge.txt");
-	Gerr4->SetTitle("V ai capi del resistore durante scarica");
-	Gerr4->GetXaxis()->SetTitle("Tempo [ms]");
+	Gerr4->SetTitle("V ai capi del resistore nel passaggio da +V0 a -V0");
+	Gerr4->GetXaxis()->SetTitle("Tempo [micro s]");
 	Gerr4->GetYaxis()->SetTitle("Tensione [V]");
 
 	
@@ -122,12 +122,41 @@ int main()
 	fit4->SetParameter(0,10);
 	fit4->SetParameter(1,50);
 
-	Gerr4->Fit(fit4,"C");
+	Gerr4->Fit(fit4,"C");	
+
+//--------------------------------------Esporta immagini
+	//c1->Print("C3_P2_RL_impulsata.eps","eps");
+
+//--------------------------------------Calcolo di tau e di L
+	//tau: mean (weighted) value and error [s]
+	double tau1 = (fit1->GetParameter(1))/1000000;
+	double wtauerr1 = 1/pow((fit1->GetParError(1))/1000000, 2);
+	double tau2 = (fit2->GetParameter(1))/1000000;
+	double wtauerr2 = 1/pow((fit2->GetParError(1))/1000000, 2);
+	double tau3 = (fit3->GetParameter(1))/1000000;
+	double wtauerr3 = 1/pow((fit3->GetParError(1))/1000000, 2);
+	double tau4 = (fit4->GetParameter(1))/1000000;
+	double wtauerr4 = 1/pow((fit4->GetParError(1))/1000000, 2);
+
+	double tau_mean = (tau1*wtauerr1+tau2*wtauerr2+tau3*wtauerr3+tau4*wtauerr4)
+						/ (wtauerr1+wtauerr2+wtauerr3+wtauerr4); 
+	double tau_err = 1/sqrt(wtauerr1+wtauerr2+wtauerr3+wtauerr4);
+
+	std::cout << "Weighted average for tau [s] = " << tau_mean <<
+				 " +/- " << tau_err << std::endl;
+
+	//resistance: value and error
+	const double resistance = 677; //Ohm
+	double resistance_err = 2 + 14870/100*0.9; //Ohm, as in Fluke manual
+	
+	//inductance: value and error (propagation)
+	double inductance = tau_mean*resistance;
+	double inductance_err = sqrt(pow(tau_err*resistance,2)+pow(tau_mean*resistance_err,2));
+
+	std::cout << "Inductance [H] = " << inductance << " +/- " << inductance_err << std::endl;
 
 	
 
-//--------------------------------------Esporta immagini
-	//c1->Print("RL_impulsata.eps","eps");
 
 
 
