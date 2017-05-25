8db6f4d7cc31b2fd089806c9fb1b7ae26eea24d5
AlfreG <alfre.guarnieri@gmail.com>
2017-05-25 19:40:18 +0100

    O1 velocità della luce istogramma

--- main.cpp
+++ main.cpp
@@ -19,32 +19,26 @@ c++ stat1.cc main.cpp -o L2.o -Wall -std=c++98 `root-config --cflags --glibs`
 #include <TCanvas.h>
 
 
-#include "./c1/src/P3_1_diod.cpp"
-#include "./c2/src/P1_1_trueRMS.cpp"
-#include "./c3/src/P1_impedenze_fdt.cpp"
-#include "./c3/src/P2_RL_impulsata.cpp"
-#include "./c3/src/P2_RC_impulsata.cpp"
-#include "./c4/src/P1_RLC_DC.cpp"
-#include "./c4/src/P2_RLC_AC.cpp"
-#include "./o3/src/funzioni.cpp"
-#include "./o3/src/P1_reticolo.cpp"
-#include "./o3/src/P2_prisma.cpp"
-#include "./o3/src/conclusioni.cpp"
+// #include "./c1/src/P3_1_diod.cpp"
+// #include "./c2/src/P1_1_trueRMS.cpp"
+// #include "./c3/src/P1_impedenze_fdt.cpp"
+// #include "./c3/src/P2_RL_impulsata.cpp"
+// #include "./c3/src/P2_RC_impulsata.cpp"
+// #include "./c4/src/P1_RLC_DC.cpp"
+// #include "./c4/src/P2_RLC_AC.cpp"
+
+// ottica
+#include "./o1/src/O1_P2.cpp"
+// #include "./o3/src/funzioni.cpp"
+// #include "./o3/src/P1_reticolo.cpp"
+// #include "./o3/src/P2_prisma.cpp"
+// #include "./o3/src/conclusioni.cpp"
 
 
 
 int main( int argc, char * argv[]){
 
 
-
-  ///////////////////////////////////////////////////////////
-  /// Istanzia finestre grafiche, canvas e pads
-
-  //TApplication* x1 = new TApplication( "x1", &argc, argv);
-  //TCanvas     * Canv0 = new TCanvas     ( "Canv0", "Canv0", 950, 950 );
-// 	 	Canv0->Divide(2,1);
-		
-		
  ///////////////////////////////////////////////////////////
  /// Laboratorio
 
@@ -68,14 +62,23 @@ int main( int argc, char * argv[]){
 //  P1_RLC_DC( Canv0 );	// C4
 //   P2_RLC_AC( Canv0 );
 
+
+
+  
+//------------ O1 --------------- // velocità della luce
+  O1_P2_c( Canv0 );
+  
+  
 //------------ O3 ---------------
 	//O3_P1_reticolo();
-	O3_P2_prisma();
+// 	O3_P2_prisma();
   	//O3_conclusioni();
 
 
   
-//  x1->Run();
+
+  
+x1->Run();
 
 
 return 0;}
